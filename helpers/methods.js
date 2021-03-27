6de588a3d7289c76c2fcf59c41f2fb95996a56d6
akrupnova <krupnova.anna83@gmail.com>
2021-03-27 13:24:15 -0700

    smokefunctional - used methods

---
+++ helpers/methods.js
@@ -0,0 +1,19 @@
+import sel from "../data/selectors";
+
+function inputValues4 (name, gender, age, story){
+    $(sel.name).setValue(name);
+    $$(sel.radioButtons)[gender].click();
+    $(sel.age).setValue(age);
+    $(sel.story).click();
+    $$(sel.storyList)[story].click();
+}
+
+// function inputValues4 (name, gender, age, story, image){
+//     $(sel.name).setValue(name);
+//     $$(sel.radioButtons)[gender].click();
+//     $(sel.age).setValue(age);
+//     $(sel.story).click();
+//     $$(sel.storyList)[story].click();
+// }
+
+module.exports = inputValues4;  //{in4, in5}
--- test/smoke/smokeFunctional.js
+++ test/smoke/smokeFunctional.js
@@ -1,5 +1,6 @@
 import sel from '../../data/selectors';
 import {name, gender, age, story} from '../../data/testData';
+import inputValues4 from '../../helpers/methods';
 
 describe('Required fields and story created', function () {
 
@@ -8,25 +9,15 @@ describe('Required fields and story created', function () {
     });
 
     it('TC-026 Submit button is enabled after fields 1-4 are field in with valid values', function () {
-        $(sel.name).setValue(name.default);
-        $$(sel.radioButtons)[gender.she].click();
-        $(sel.age).setValue(age.default);
-        $(sel.story).click();
-        $$(sel.storyList)[story.comedy].click();
+        inputValues4(name.default, gender.she, age.default, story.comedy);
         let submitBtn = $(sel.submit).isEnabled();
         expect(submitBtn).toEqual(true);
     });
 
     it('TC-027 User is redirected to the story page', function () {
         browser.refresh();
-
-        $(sel.name).setValue(name.default);
-        $$(sel.radioButtons)[gender.she].click();
-        $(sel.age).setValue(age.default);
-        $(sel.story).click();
-        $$(sel.storyList)[story.comedy].click();
+        inputValues4(name.default, gender.she, age.default, story.comedy);
         $(sel.submit).click();
-
         let tryAgainBtn = $(sel.tryAgain).isDisplayed();
         expect(tryAgainBtn).toEqual(true);
     });
